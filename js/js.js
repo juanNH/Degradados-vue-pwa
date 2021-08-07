307838fa8ee4001982c9b04a0d504aabdddaa237
Juan Herrera <juannicolash01@gmail.com>
2021-08-07 12:31:37 -0300

    refactorizacion de codigo, arreglo de carpetas, position

--- js/js.js
+++ js/js.js
@@ -3,10 +3,21 @@ const app = Vue.createApp({
         title:'Generador de degradados',
         color1:'#000000',
         color2:'#000000',
+        orientation: 4,
+        position: 'to right',
     }),
     computed: {
         setColor(){
-            return `background:linear-gradient(to right, ${this.color1}, ${this.color2});width:100%;height:300px;`;
+            if(this.orientation == 1) {
+                this.position = 'to right';
+            }else if(this.orientation == 2) {
+                this.position = 'to left';
+            }else if(this.orientation == 3) {
+                this.position = 'to top';
+            }else {
+                this.position = 'to bottom';
+            }
+            return `background:linear-gradient(${this.position}, ${this.color2}, ${this.color1});`;
         },
     },
 });
