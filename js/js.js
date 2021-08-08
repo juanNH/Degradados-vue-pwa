d5a9d387adb95a8c06217441c64a0c3a98c59e6c
Juan Herrera <juannicolash01@gmail.com>
2021-08-08 12:54:06 -0300

    add reset de position sin check

--- js/js.js
+++ js/js.js
@@ -10,14 +10,18 @@ const app = Vue.createApp({
     }),
     computed: {
         setColor(){
-            if(this.orientation == 1) {
-                this.position = 'to right';
-            }else if(this.orientation == 2) {
-                this.position = 'to left';
-            }else if(this.orientation == 3) {
-                this.position = 'to top';
-            }else if(this.orientation == 4) {
-                this.position = 'to bottom';
+            if(this.disabled == 1) {
+                if(this.orientation == 1) {
+                    this.position = 'to right';
+                }else if(this.orientation == 2) {
+                    this.position = 'to left';
+                }else if(this.orientation == 3) {
+                    this.position = 'to top';
+                }else if(this.orientation == 4) {
+                    this.position = 'to bottom';
+                }
+            }else{
+                this.position = 0
             }
             return `background:linear-gradient(${this.position}, ${this.colors});`;
         },
